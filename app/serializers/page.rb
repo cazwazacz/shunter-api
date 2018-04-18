fc7f5bca7345c710407900f230dd9cf4e6606835
WAZACZ <cazwazacz@gmail.com>
2018-04-18 11:31:56 +0100

    Create to_h and components methods

--- app/serializers/page.rb
+++ app/serializers/page.rb
@@ -2,8 +2,23 @@ module Serializers
   class Page < Base
     def to_h
       {
-
+          layout: { template: 'layout' },
+          components: components
       }
+    end
+
+    def components
+      c = []
+
+      c << header
+      c << content
+      c << footer
+
+      c.flatten
+    end
+
+    def header
+
     end
   end
 end
