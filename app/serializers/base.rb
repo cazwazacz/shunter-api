5763a450baf227b2d4d876843390e1480d2c6f7a
WAZACZ <cazwazacz@gmail.com>
2018-04-18 11:26:36 +0100

    Empty Base serializer

--- app/serializers/base.rb
+++ app/serializers/base.rb
@@ -1,42 +1,11 @@
 module Serializers
   class Base
-
-    def initialize(options)
-      @options = options
+    def to_h
+      content
     end
 
-    def produce_json
-      remove_components(base_json, @options)
+    def content
+      {}
     end
-
-    def base_json
-      {}.tap do |hash|
-        hash["components"] = []
-        push_to_components(hash["components"], "cookie-banner", "cookie-banner")
-        push_to_components(hash["components"], "top-navigation", "top-navigation")
-        # push_to_components(hash["components"], "banner", "banner")
-        push_to_components(hash["components"], "banner", "banner")
-        push_to_components(hash["components"], "header", "header")
-          # push_to_components(hash["components"], "footer", "footer")
-      end
-    end
-
-    def add_footer_to_json(json_hash)
-      json_hash.tap do |hash|
-        push_to_components(hash["components"], "footer", "footer")
-      end
-    end
-
-    def remove_components(hash, options = {})
-      options.each do |key, value|
-        hash["components"].delete_if { |element| (element[:name] == key.to_s && value == false) }
-      end
-      hash
-    end
-
-    def push_to_components(hash, name, data)
-      hash << { name: name, data: data }
-    end
-
   end
 end
