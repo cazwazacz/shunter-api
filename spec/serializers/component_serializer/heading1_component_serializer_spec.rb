25612acc8a5f09be17d8384e762db982b5b10c39
WAZACZ <cazwazacz@gmail.com>
2018-04-20 11:12:21 +0100

    Create image component spec

--- spec/serializers/component_serializer/heading1_component_serializer_spec.rb
+++ spec/serializers/component_serializer/heading1_component_serializer_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../../rails_helper'
 
 describe ComponentSerializer::Heading1ComponentSerializer do
-  let(:person) { double('person', full_name: "Diane Abbott") }
+  let(:person) { double('person', full_name: 'Diane Abbott') }
 
   let (:serializer) { described_class.new(person) }
 
   context '#to_h' do
     it 'returns a hash with the name and data' do
-      hash = { name: "heading1", data: "Diane Abbott" }
+      hash = { name: 'heading1', data: 'Diane Abbott' }
       expect(serializer.to_h).to eq hash
     end
   end
---
+++ spec/serializers/component_serializer/image_component_serializer_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../../rails_helper'
+
+describe ComponentSerializer::ImageComponentSerializer do
+  let(:object) { double('object', graph_id: '321', display_name: 'Diane Abbott', image_id: 123) }
+
+  let (:serializer) { described_class.new(object) }
+
+  context '#to_h' do
+    it 'returns a hash with the name and data' do
+      data = {
+          template: 'person-image',
+          'figure-url': '/media/321',
+          'image-srcset1': 'https://api.parliament.uk/Staging/photo/123.jpeg?crop=CU_5:2&width=732&quality=80, https://api.parliament.uk/Staging/photo/123.jpeg?crop=CU_5:2&width=1464&quality=80 2x',
+          'image-srcset2': 'https://api.parliament.uk/Staging/photo/123.jpeg?crop=MCU_3:2&width=444&quality=80, https://api.parliament.uk/Staging/photo/123.jpeg?crop=MCU_3:2&width=888&quality=80 2x',
+          'image-src': 'https://api.parliament.uk/Staging/photo/123.jpeg?crop=CU_1:1&width=186&quality=80',
+          'image-alt': 'Diane Abbott'
+      }
+      hash = { name: 'image', data: data }
+      expect(serializer.to_h).to eq hash
+    end
+  end
+end
