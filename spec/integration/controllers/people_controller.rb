830b46581a3c488d0d09fe2e0d20c542bbca7dc8
WAZACZ <cazwazacz@gmail.com>
2018-04-24 16:05:44 +0100

    Create specs for people#show JSON for former mp and current lord

--- spec/integration/controllers/people_controller.rb
+++ spec/integration/controllers/people_controller.rb
@@ -2,14 +2,29 @@ require_relative '../../rails_helper'
 
   RSpec.describe PeopleController, vcr: true do
     describe 'GET show' do
-      before(:each) do
-        get '/people/43RHonMf'
-      end
-
       context 'with a person that exists' do
         context 'with all the information present' do
           it 'renders expected JSON output' do
+            get '/people/43RHonMf'
             expected_json = get_fixture('people_controller/show/with_all_data.json')
+
+            expect(response.body).to eq(expected_json)
+          end
+        end
+
+        context 'for a former mp' do
+          it 'renders expected JSON output' do
+            get '/people/qkVSY7jb'
+            expected_json = get_fixture('people_controller/show/former_mp.json')
+
+            expect(response.body).to eq(expected_json)
+          end
+        end
+
+        context 'for a current lord' do
+          it 'renders expected JSON output' do
+            get '/people/vRffoNWN'
+            expected_json = get_fixture('people_controller/show/current_lord.json')
 
             expect(response.body).to eq(expected_json)
           end
