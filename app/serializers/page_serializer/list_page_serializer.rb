0bf7ec7686268d914ce7fb9e6b63d3bb852b9480
WAZACZ <cazwazacz@gmail.com>
2018-04-24 10:31:35 +0100

    active_letter defaults to 'all' for use in people#index

--- app/serializers/page_serializer/list_page_serializer.rb
+++ app/serializers/page_serializer/list_page_serializer.rb
@@ -1,6 +1,6 @@
 module PageSerializer
   class ListPageSerializer < PageSerializer::BasePageSerializer
-    def initialize(objects, klass, objects_name, letters, active_letter)
+    def initialize(objects, klass, objects_name, letters, active_letter = 'all')
       @objects = objects
       @klass = klass
       @object_name = objects_name
@@ -11,7 +11,11 @@ module PageSerializer
     private
 
     def title
-      "#{@object_name.capitalize} A to Z showing results for #{@active_letter.capitalize} - UK Parliament"
+      if @active_letter != 'all'
+        "#{@object_name.capitalize} A to Z showing results for #{@active_letter.capitalize} - UK Parliament"
+      else
+        'People - UK Parliament' if @active_letter == 'all'
+      end
     end
 
     def content
