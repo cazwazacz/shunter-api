92518178af0fa2ab6be53e341256f602fd21f0c9
WAZACZ <cazwazacz@gmail.com>
2018-04-26 14:33:28 +0100

    Mody former_seat_incumbencies so only former MPs are selected

--- app/serializers/component_serializer/former_seat_incumbencies_component_serializer.rb
+++ app/serializers/component_serializer/former_seat_incumbencies_component_serializer.rb
@@ -15,14 +15,12 @@ module ComponentSerializer
     end
 
     def former_seat_incumbencies
-      @seat_incumbencies.map do |seat_incumbency|
-        if !seat_incumbency.current?
-          {
-            member_name: seat_incumbency.member.display_name,
-            graph_id: seat_incumbency.member.graph_id,
-            date_range: seat_incumbency.date_range
-          }
-        end
+      @seat_incumbencies.select { |seat_incumbency| seat_incumbency.current? == false }.map do |seat_incumbency|
+        {
+          member_name: seat_incumbency.member.display_name,
+          graph_id: seat_incumbency.member.graph_id,
+          date_range: seat_incumbency.date_range
+        }
       end
     end
   end
