7a747a5db18cc612defb9bf911fbf9fca24b20c3
WAZACZ <cazwazacz@gmail.com>
2018-04-26 12:13:32 +0100

    Add Person component to people array in list of components

--- app/serializers/page_serializer/constituency_show_page_serializer.rb
+++ app/serializers/page_serializer/constituency_show_page_serializer.rb
@@ -18,9 +18,12 @@ module PageSerializer
       c << ComponentSerializer::ConstituencyHeadingComponentSerializer.new(@constituency).to_h
       c << ComponentSerializer::ConstituencySubheadingComponentSerializer.new(@constituency).to_h
       c << ComponentSerializer::MapComponentSerializer.new(@json_location).to_h
-      c << ComponentSerializer::PersonComponentSerializer.new(
-          @member, { constituency_show_page: true, constituency_name: @constituency.name }
-      ).to_h
+      c << {
+          name: "people",
+          data: [ComponentSerializer::PersonComponentSerializer.new(
+              @member, { constituency_show_page: true, constituency_name: @constituency.name }
+          ).to_h]
+      }
 
       c
     end
