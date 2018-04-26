985c387e28a9d6ba8dbd3d1c501d806fe378d958
WAZACZ <cazwazacz@gmail.com>
2018-04-26 17:02:48 +0100

    Create integration specs for ConstituenciesController

---
+++ spec/integration/controllers/constituencies_controller_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../../rails_helper'
+
+RSpec.describe ConstituenciesController, vcr: true do
+  describe 'GET show' do
+    context 'with a constituency that exists' do
+      it 'renders expected JSON output' do
+        get '/constituencies/yyocmekJ'
+        expected_json = get_fixture('constituencies_controller/show/with_all_data.json')
+
+        expect(response.body).to eq(expected_json)
+      end
+    end
+  end
+
+  describe 'GET map' do
+    context 'with a constituency that exists' do
+      it 'renders expected JSON output' do
+        get '/constituencies/yyocmekJ/map.json'
+        expected_json = get_fixture('constituencies_controller/map/map.json')
+
+        expect(response.body).to eq(expected_json)
+      end
+    end
+  end
+end
---
+++ spec/integration/controllers/people_controller_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../../rails_helper'
+
+RSpec.describe PeopleController, vcr: true do
+  describe 'GET show' do
+    context 'with a person that exists' do
+      context 'for a current mp' do
+        it 'renders expected JSON output' do
+          get '/people/43RHonMf'
+          expected_json = get_fixture('people_controller/show/with_all_data.json')
+
+          expect(response.body).to eq(expected_json)
+        end
+      end
+
+      context 'for a former mp' do
+        it 'renders expected JSON output' do
+          get '/people/qkVSY7jb'
+          expected_json = get_fixture('people_controller/show/former_mp.json')
+
+          expect(response.body).to eq(expected_json)
+        end
+      end
+
+      context 'for a current lord' do
+        it 'renders expected JSON output' do
+          get '/people/vRffoNWN'
+          expected_json = get_fixture('people_controller/show/current_lord.json')
+
+          expect(response.body).to eq(expected_json)
+        end
+      end
+
+      context 'for a former lord' do
+        it 'renders expected JSON output' do
+          get '/people/O0giLg8A'
+          expected_json = get_fixture('people_controller/show/former_lord.json')
+
+          expect(response.body).to eq(expected_json)
+        end
+      end
+    end
+  end
+end
