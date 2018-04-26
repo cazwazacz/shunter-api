97a77653bfcc537ee9cf65d2512fc88c53b1adaf
WAZACZ <cazwazacz@gmail.com>
2018-04-26 11:20:14 +0100

    Use PersonComponentSerializer in ConstituencyShowPageSerializer

--- app/serializers/page_serializer/constituency_show_page_serializer.rb
+++ app/serializers/page_serializer/constituency_show_page_serializer.rb
@@ -1,8 +1,9 @@
 module PageSerializer
   class ConstituencyShowPageSerializer < PageSerializer::BasePageSerializer
-    def initialize(constituency, json_location)
+    def initialize(constituency, json_location, member)
       @constituency = constituency
       @json_location = json_location
+      @member = member
     end
 
     private
@@ -17,6 +18,9 @@ module PageSerializer
       c << ComponentSerializer::ConstituencyHeadingComponentSerializer.new(@constituency).to_h
       c << ComponentSerializer::ConstituencySubheadingComponentSerializer.new(@constituency).to_h
       c << ComponentSerializer::MapComponentSerializer.new(@json_location).to_h
+      c << ComponentSerializer::PersonComponentSerializer.new(
+          @member, { constituency_show_page: true, constituency_name: @constituency.name }
+      ).to_h
 
       c
     end
