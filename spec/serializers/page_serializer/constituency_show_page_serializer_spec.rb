71016b33886dd6b096df2b2b2fe6d31ef7e18961
WAZACZ <cazwazacz@gmail.com>
2018-04-26 11:27:54 +0100

    Update ConstituencyShowPageSerializer spec

--- spec/serializers/page_serializer/constituency_show_page_serializer_spec.rb
+++ spec/serializers/page_serializer/constituency_show_page_serializer_spec.rb
@@ -2,21 +2,25 @@ require_relative '../../rails_helper'
 
 RSpec.describe PageSerializer::ConstituencyShowPageSerializer do
 
+  let(:member) { double('member') }
   let(:json_location) { double('json_location') }
   let(:constituency) { double('constituency', name: 'Bath') }
-  let(:serializer) { described_class.new(constituency, json_location) }
+  let(:serializer) { described_class.new(constituency, json_location, member) }
 
   context '#to_h' do
     it 'initialises all serializers correctly' do
       allow(ComponentSerializer::ConstituencyHeadingComponentSerializer).to receive(:new)
       allow(ComponentSerializer::ConstituencySubheadingComponentSerializer).to receive(:new)
       allow(ComponentSerializer::MapComponentSerializer).to receive(:new)
+      allow(ComponentSerializer::PersonComponentSerializer).to receive(:new)
 
       serializer.to_h
 
       expect(ComponentSerializer::ConstituencyHeadingComponentSerializer).to have_received(:new).with(constituency)
       expect(ComponentSerializer::ConstituencySubheadingComponentSerializer).to have_received(:new).with(constituency)
       expect(ComponentSerializer::MapComponentSerializer).to have_received(:new).with(json_location)
+      options = { constituency_show_page: true, constituency_name: 'Bath' }
+      expect(ComponentSerializer::PersonComponentSerializer).to have_received(:new).with(member, options)
     end
   end
 
