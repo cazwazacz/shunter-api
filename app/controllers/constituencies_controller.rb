99aaabd302f5b1f0013f8877b83f8af317cf1393
WAZACZ <cazwazacz@gmail.com>
2018-04-27 16:47:24 +0100

    Sort constituencies by name

--- app/controllers/constituencies_controller.rb
+++ app/controllers/constituencies_controller.rb
@@ -67,11 +67,9 @@ class ConstituenciesController < ApplicationController
     end
   end
 
-
-
   def letters
     @constituencies, @letters = Parliament::Utils::Helpers::FilterHelper.filter_sort(@request, :sort_name, 'ConstituencyGroup', ::Grom::Node::BLANK)
-    @constituencies = @constituencies.select { |constituency| constituency.current? }
+    @constituencies = @constituencies.select { |constituency| constituency.current? }.sort_by { |constituency| constituency.name }
 
     render_page(PageSerializer::ListPageSerializer.new(
         @constituencies,
