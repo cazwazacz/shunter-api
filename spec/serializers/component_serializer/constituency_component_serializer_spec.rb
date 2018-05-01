5bd51b7b7b24517088a028d4f1880e8086ab239d
WAZACZ <cazwazacz@gmail.com>
2018-05-01 15:30:42 +0100

    Update spec

--- spec/serializers/component_serializer/constituency_component_serializer_spec.rb
+++ spec/serializers/component_serializer/constituency_component_serializer_spec.rb
@@ -2,27 +2,61 @@ require_relative '../../rails_helper'
 
 RSpec.describe ComponentSerializer::ConstituencyComponentSerializer do
 
-  let (:constituency) {
+  let (:current_constituency) {
     double(
         'constituency',
+        current?: true,
         name: 'Falkirk',
         graph_id: 'Eda5tpUZ',
         current_member_display_name: 'John McNally',
         current_member_party_name: 'Scottish National Party'
     )
   }
-  let (:serializer) { described_class.new(constituency) }
+
+  let (:former_constituency) {
+    double(
+        'constituency',
+        current?: false,
+        name: 'Falkirk',
+        graph_id: 'Eda5tpUZ',
+        start_date: double('start_date', year: 1945),
+        end_date: double('end_date', year: 2010)
+    )
+  }
+
+  let (:current_constituency_component_serializer) { described_class.new(current_constituency) }
+  let (:former_constituency_component_serializer) { described_class.new(former_constituency) }
 
   context '#to_h' do
-    it 'returns constituency component' do
-      hash = {
-          'constituency_name': 'Falkirk',
-          'graph_id': 'Eda5tpUZ',
-          'current_member': 'John McNally',
-          'party': 'Scottish National Party'
-      }
+    context 'if the constituency is current' do
+      it 'returns the constituency component' do
+        hash = {
+            current: true,
+            constituency_hash: {
+              'constituency_name': 'Falkirk',
+              'graph_id': 'Eda5tpUZ',
+              'current_member': 'John McNally',
+              'party': 'Scottish National Party'
+            }
+        }
 
-      expect(serializer.to_h).to eq hash
+        expect(current_constituency_component_serializer.to_h).to eq hash
+      end
+    end
+
+    context 'if the constituency is former' do
+      it 'returns the constituency component' do
+        hash = {
+            current: false,
+            constituency_hash: {
+                'constituency_name': 'Falkirk (1945 - 2010)',
+                'graph_id': 'Eda5tpUZ',
+                'subtitle': 'Former constituency'
+            }
+        }
+
+        expect(former_constituency_component_serializer.to_h).to eq hash
+      end
     end
   end
 end
